ec81f4cb7ad4a36889ce37fc7769fc14a1607e90
Aleksandr Sayutin <alex.sautin@mail.ru>
2018-08-25 20:01:59 +0600

    Задание №1. Добивил тест, который открывает и закрывет браузер.

---
+++ SelenuimExample/SelenuimExample/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumExamples
+{
+    [TestFixture]
+    public class MyFirstTest
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void Start()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+
+        [Test]
+        public void FirstTest()
+        {
+            driver.Url = "http://www.google.com/";
+            driver.FindElement(By.Name("q")).SendKeys("webdriver");
+        }
+
+        [TearDown]
+        public void Finish()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
