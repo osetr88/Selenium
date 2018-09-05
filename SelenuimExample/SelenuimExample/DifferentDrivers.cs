03d1777e4d1992a388bfb00218ae246314f24d9b
Aleksandr Sayutin <alex.sautin@mail.ru>
2018-09-05 21:22:00 +0600

    Задание №4. Добивил сценариb для логина через разные браузеры.

---
+++ SelenuimExample/SelenuimExample/DifferentDrivers.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace SelenuimExample
+{
+    [TestFixture]
+    class DifferentDrivers
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void Start()
+        {
+            
+        }
+
+        [Test]
+        public void FireFoxLogin()
+        {
+            driver = new FirefoxDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+            driver.Url = "http://localhost/litecart/admin/";
+            driver.FindElement(By.Name("username")).SendKeys("admin");
+            driver.FindElement(By.Name("password")).SendKeys("admin");
+            driver.FindElement(By.Name("login")).Click();
+        }
+
+        [Test]
+        public void IELogin()
+        {
+            driver = new InternetExplorerDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            
+            driver.Url = "http://localhost/litecart/admin/";
+            driver.FindElement(By.Name("username")).SendKeys("admin");
+            driver.FindElement(By.Name("password")).SendKeys("admin");
+            driver.FindElement(By.Name("login")).Click();
+        }
+
+        [TearDown]
+        public void Finish()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
